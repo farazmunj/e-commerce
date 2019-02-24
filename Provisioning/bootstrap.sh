c3cbbe89015d5c7c5911f8f9037c0682f41c3a6a
faraz <farazilu@gmail.com>
2019-02-24 15:06:29 +0000

    update provision and save mysql connection settings.

--- Provisioning/bootstrap.sh
+++ Provisioning/bootstrap.sh
@@ -28,23 +28,23 @@ installMysql(){
 	printLog "Start mysql server";	
 	sudo systemctl start mysql
 	printLog "Precona password";
-	cat /var/log/mysqld.log | grep root@localhost: > ~/_last
-	awk 'NF>1{print $NF}' ~/_last > ~/precona.password
-	mysql -u root -p`cat ~/precona.password` -e "alter user 'root'@'localhost' identified by 'RootPw90$' " --connect-expired-password
-	mysql -u root -p'RootPw90$' -e "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))"
-	rm ~/_last
-	rm ~/precona.password
+	#cat /var/log/mysqld.log | grep root@localhost: > ~/_last
+	#awk 'NF>1{print $NF}' ~/_last > ~/precona.password
+	#mysql -u root -p`cat ~/precona.password` -e "alter user 'root'@'localhost' identified by 'RootPw90$' " --connect-expired-password
+	#mysql -u root  -e "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))"
+	#rm ~/_last
+	#rm ~/precona.password
 }
 
 importDB(){
 	printLog "Import DB";
-	mysql -u root -p'RootPw90$' -e "create database system_core"
+	sudo mysql -u root -e "create database system_core"
 
-	mysql -u root -p'RootPw90$' -e "create user 'system_core'@'localhost' identified by 'Nokia82!0' "
-	mysql -u root -p'RootPw90$' -e "create user 'system_core'@'%' identified by 'Nokia82!0' "
+	sudo mysql -u root -e "create user 'system_core'@'localhost' identified by 'Nokia82!0' "
+	sudo mysql -u root -e "create user 'system_core'@'%' identified by 'Nokia82!0' "
 
-	mysql -u root -p'RootPw90$' -e "GRANT ALL PRIVILEGES on * . * to 'system_core'@'localhost' "
-	mysql -u root -p'RootPw90$' -e "GRANT ALL PRIVILEGES on * . * to 'system_core'@'%' "
+	sudo mysql -u root -e "GRANT ALL PRIVILEGES on * . * to 'system_core'@'localhost' "
+	sudo mysql -u root -e "GRANT ALL PRIVILEGES on * . * to 'system_core'@'%' "
 
 	#mysql --host localhost --user=root --password='RootPw90$' system_core < system_core.sql
 }
@@ -53,6 +53,18 @@ installPHP(){
 
 	printLog "Install PHP"
 	sudo apt --assume-yes install php
+
+sudo echo '<VirtualHost *:80>
+
+	#ServerName www.example.com
+
+	ServerAdmin webmaster@localhost
+	DocumentRoot /vagrant
+
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+'  > /etc/apache2/sites-available/000-default.conf
 }
 
 
@@ -60,11 +72,10 @@ printLog "Setting Timzone for host";
 mv /etc/localtime /etc/localtime.orig
 ln -s $TIME_ZONE_FILE /etc/localtime
 
-
-
+sudo apt-get --assume-yes update
 printLog "Install MC"
-sudo apt get --assume-yes install mc 
-sudo apt get --assume-yes install wget
+sudo apt --assume-yes install mc 
+sudo apt --assume-yes install wget
 
 
 
@@ -79,5 +90,5 @@ installMysql;
 importDB;
 
 printLog "Restart apache";
-sudo /etc/init.d/mysql restart
+sudo service mysql restart
 sudo service apache2 restart
