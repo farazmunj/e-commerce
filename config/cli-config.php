a4cdef78a23761831bb02889bc6e1f3e8741d2f9
faraz <farazilu@gmail.com>
2019-02-24 22:34:31 +0000

    add doctrine as ORM propel is not working with php7.2

---
+++ config/cli-config.php
@@ -0,0 +1,15 @@
+<?php
+
+// cli-config.php
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Slim\Container;
+
+/** @var Container $container */
+$container = require_once __DIR__ . '/bootstrap.php';
+
+ConsoleRunner::run(
+    ConsoleRunner::createHelperSet($container[EntityManager::class])
+);
+
--- src/settings.php
+++ src/settings.php
@@ -1,4 +1,5 @@
 <?php
+define('APP_ROOT', __DIR__);
 return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
@@ -6,14 +7,37 @@ return [
 
         // Renderer settings
         'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
+            'template_path' => __DIR__ . '/../templates/'
         ],
 
         // Monolog settings
         'logger' => [
             'name' => 'slim-app',
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
+            'level' => \Monolog\Logger::DEBUG
         ],
-    ],
+        'doctrine' => [
+            // if true, metadata caching is forcefully disabled
+            'dev_mode' => true,
+
+            // path where the compiled metadata info will be cached
+            // make sure the path exists and it is writable
+            'cache_dir' => APP_ROOT . '/var/doctrine',
+
+            // you should add any other path containing annotated entity classes
+            'metadata_dirs' => [
+                APP_ROOT . '/src/Domain'
+            ],
+
+            'connection' => [
+                'driver' => 'pdo_mysql',
+                'host' => 'localhost',
+                'port' => 3306,
+                'dbname' => 'system_core',
+                'user' => 'system_core',
+                'password' => 'Nokai82!0',
+                'charset' => 'utf-8'
+            ]
+        ]
+    ]
 ];
